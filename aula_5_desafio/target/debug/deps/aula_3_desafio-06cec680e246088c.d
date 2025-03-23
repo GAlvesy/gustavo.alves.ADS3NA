61412db51165d882fcf6c2457523e6eb8d5a7387
GAlvesy <alvesdealmeidagustavo@gmail.com>
2025-03-23 21:43:38 +0000

    desafio dado, desafio cumprido

---
+++ aula_5_desafio/target/debug/deps/aula_3_desafio-06cec680e246088c.d
@@ -0,0 +1,5 @@
+/workspaces/gustavo.alves.ADS3NA/aula_3_desafio/target/debug/deps/aula_3_desafio-06cec680e246088c: src/lib.rs
+
+/workspaces/gustavo.alves.ADS3NA/aula_3_desafio/target/debug/deps/aula_3_desafio-06cec680e246088c.d: src/lib.rs
+
+src/lib.rs:
---
+++ aula_5_desafio/target/debug/libaula_3_desafio.d
@@ -0,0 +1,1 @@
+/workspaces/gustavo.alves.ADS3NA/aula_3_desafio/target/debug/libaula_3_desafio.rlib: /workspaces/gustavo.alves.ADS3NA/aula_3_desafio/src/lib.rs
